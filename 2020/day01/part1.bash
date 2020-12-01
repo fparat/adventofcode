9a5e820b66c280e7f7c49570db369a037b89fd1f
Franck Parat <franck.parat@gmail.com>
2020-12-01 14:54:51 +0100

    2020 day 1 part 1 (Bash)

---
+++ 2020/day01/part1.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+num_lines=$(wc -l input | awk '{print $1}')
+
+a_iter=$num_lines
+b_iter=$num_lines
+
+for a in $(<input); do
+    a_iter=$(( $a_iter - 1))
+    b_iter=$a_iter
+    for b in $(tail -n$a_iter input); do
+        b_iter=$(( $b_iter - 1 ))
+        if [[ "$(( $a + $b ))" == "2020" ]] ; then
+            echo "Result: $(( $a * $b ))"
+            exit
+        fi
+    done
+done
