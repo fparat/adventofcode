ca4db4f1bce57216fab0e4b1cf5843514d6dd446
franck.parat@gmail.com <franck.parat@gmail.com>
2022-12-05 00:12:44 +0100

    2022 day 2 part 1 (Python)

---
+++ 2022/day02/part1.py
@@ -0,0 +1,25 @@
+import sys
+
+if __name__ == "__main__":
+    try:
+        filename = sys.argv[1]
+    except IndexError:
+        filename = "input"
+
+    with open(filename) as f:
+        inp = f.read()
+
+    # Part 1
+    rounds = inp.splitlines()
+    scores = [{
+        "A X": 1 + 3,
+        "A Y": 2 + 6,
+        "A Z": 3 + 0,
+        "B X": 1 + 0,
+        "B Y": 2 + 3,
+        "B Z": 3 + 6,
+        "C X": 1 + 6,
+        "C Y": 2 + 0,
+        "C Z": 3 + 3,
+    }[round] for round in rounds]
+    print(f"Part1: {sum(scores)}")
