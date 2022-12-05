9c16d0cca149a537ced6c0d514acae568cff84c9
franck.parat@gmail.com <franck.parat@gmail.com>
2022-12-06 00:29:40 +0100

    2022 day 5 part 1 (Python)

---
+++ 2022/day05/part1.py
@@ -0,0 +1,64 @@
+import sys
+
+def draw(stacks):
+    stacks = [stack[:] for stack in stacks]
+    max_len = max(len(stack) for stack in stacks)
+    lines = ["".join(f" {i+1}  " for i in range(len(stacks)))]
+    for i in range(max_len):
+        crates = []
+        for stack in stacks:
+            try:
+                crate = f"[{stack.pop(0)}] "
+            except IndexError:
+                crate = "    "
+            crates.append(crate)
+        line = "".join(crates)
+        lines.append(line)
+
+    for line in reversed(lines):
+        print(line)
+
+
+if __name__ == "__main__":
+    try:
+        filename = sys.argv[1]
+    except IndexError:
+        filename = "input"
+
+    with open(filename) as f:
+        inp = f.read()
+
+    # Part 1
+    stacks_draw, procedure = inp.split("\n\n")
+    stacks_lines = stacks_draw.splitlines()
+    stack_num = len(stacks_lines.pop().split())
+
+    stacks = [[] for _ in range(stack_num)]
+    for line in reversed(stacks_lines):
+        for i in range(stack_num):
+            # [L] [C] [W]
+            # 0123456789
+            pos = 4 * i + 1
+            crate = line[pos]
+            if crate.strip():
+                stacks[i].append(crate)
+
+    instructions = []
+    for line in procedure.splitlines():
+        # move 2 from 5 to 9
+        toks = line.split()
+        num = int(toks[1])
+        start = int(toks[3]) - 1
+        end = int(toks[5]) - 1
+        instructions.append((num, start, end))
+
+    draw(stacks)
+    for num, start, end in instructions:
+        print(f"move {num} from {start} to {end}")
+        for i in range(num):
+            crate = stacks[start].pop()
+            stacks[end].append(crate)
+        draw(stacks)
+
+    tops = "".join([stack[-1] for stack in stacks])
+    print(f"Part 1: {tops}")
