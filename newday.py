d36d288f85028ae96ce426f5f4ba51f62f68ca8f
franck.parat@gmail.com <franck.parat@gmail.com>
2022-12-05 22:36:29 +0100

    Add newday.py and python template

---
+++ newday.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import argparse
+import subprocess
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+_dry_run = False
+
+
+def sh(cmd):
+    print(f"> {cmd}")
+    if not _dry_run:
+        subprocess.run(cmd, shell=True, check=True)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("year", type=int)
+    parser.add_argument("day", type=int)
+    parser.add_argument("--lang", "-l", default="none")
+    parser.add_argument("--dry-run", "-n", action="store_true")
+    args = parser.parse_args()
+
+    year = str(int(args.year))
+    if int(year) < 2015:
+        raise ValueError(f"invalid year {year}")
+
+    day = str(int(args.day))
+    if not 1 <= int(day) <= 25:
+        raise ValueError(f"invalid day {day}")
+
+    _dry_run = args.dry_run
+
+    day_path = os.path.join(SCRIPT_DIR, year, f"day{day:>02}")
+    day_relpath = os.path.relpath(day_path, SCRIPT_DIR)
+
+    link_path = os.path.join(SCRIPT_DIR, "current")
+
+    template_path = os.path.join(SCRIPT_DIR, "templates", args.lang)
+    if args.lang == "none":
+        template_path = None
+    elif not os.path.isdir(template_path):
+        raise ValueError(f'invalid lang {args.lang}, "templates/<lang>" must be a dir')
+
+    sh(f"mkdir -vp {day_path}")
+    sh(f"rm {link_path}")
+    sh(f"ln -s {day_relpath} {link_path}")
+
+    if template_path:
+        sh(f"cp -vr {template_path}/* {day_path}/")
---
+++ templates/python/part1.py
@@ -0,0 +1,12 @@
+import sys
+
+if __name__ == "__main__":
+    try:
+        filename = sys.argv[1]
+    except IndexError:
+        filename = "input"
+
+    with open(filename) as f:
+        inp = f.read()
+
+    # Part 1
