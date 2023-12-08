95a0e74ef0d2902883ba351a55b412f4c9da3d28
Franck Parat <franck.parat@gmail.com>
2023-12-08 20:24:16 +0100

    2023 day 8 part 2 (Python)

---
+++ 2023/day08/part2.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+import itertools
+import math
+
+
+if __name__ == "__main__":
+    try:
+        filename = sys.argv[1]
+    except IndexError:
+        filename = "input"
+
+    with open(filename) as f:
+        inp = f.read()
+
+    # Part 1
+
+    lines = inp.splitlines()
+    instructions = [{"L": 0, "R": 1}[s] for s in lines[0]]
+
+    network = {}
+    for node, left, right in (re.match(r"(\w+)\s*=\s*\((\w+),\s*(\w+)\)\s*", line).groups() for line in lines[2:]):
+        network[node] = (left, right)
+
+    step = 0
+    node = "AAA"
+    instr = itertools.cycle(instructions)
+    while node != "ZZZ":
+        node = network[node][next(instr)]
+        step += 1
+
+    print(f"Part 1: {step}")
+
+    # Part 2
+
+    def cost(node):
+        step = 0
+        instr = itertools.cycle(instructions)
+        while not node.endswith("Z"):
+            node = network[node][next(instr)]
+            step += 1
+        return step
+
+    cycles = {start: cost(start) for start in [node for node in network.keys() if node.endswith("A")]}
+
+    print(f"Part 2: {math.lcm(*cycles.values())}")
