487a2ab75e73dbb7620d8557c4006b1fe69b60de
Franck Parat <franck.parat@gmail.com>
2024-12-02 17:40:05 +0100

    Improve Rust template

--- templates/rust/part1.rs
+++ templates/rust/part1.rs
@@ -1,5 +1,9 @@
 fn main() {
-    let part1 = std::fs::read_to_string("input")
+    let filename = std::env::args()
+        .nth(1)
+        .unwrap_or_else(|| "input".to_string());
+
+    let part1 = std::fs::read_to_string(&filename)
         .unwrap()
         .lines()
         .count();
